d46e15159b76d6977d6223f58abf70237b0ab96e
AdityaManikanth2810 <adityamanikanthrao@gmail.com>
2020-08-27 01:03:21 +0530

    Added Google auth and navbar

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const connectDB = require('./config/db');
 
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 
 
 //Load Configuration
 dotenv.config({path: './config/config.env'})
+
+//Passport Config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -26,8 +31,20 @@ app.use(express.static(path.join(__dirname,'public')));
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+//Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
